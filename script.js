adf4bc1efbf40a809d6bd3736336ad11b5991de3
pranavtharoor <pranav.tharoor@gmail.com>
2018-09-18 18:24:35 +0530

    Modified scrolling

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ window.onload = () => {
       sections
         .filter(section => section.id === e.target.getAttribute('data-scroll'))[0]
         .element
-        .scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+        .scrollIntoView({ behavior: "smooth", block: "end", inline: "start" });
       currentSection = sections.map(section => section.id).indexOf(e.target.getAttribute('data-scroll'));
       document.querySelector('.to-top').style.display = 'block';
       if(![...document.querySelector('nav').classList].includes('nav-mobile'))
@@ -15,7 +15,7 @@ window.onload = () => {
       document.querySelector('.light-box').style.opacity = 0;
       document.querySelector('nav').style.width = 0;
       [...document.querySelector('.menu-button').children].forEach(el => {
-        el.style.backgroundColor = !!sections[currentSection].slide ? 'var(--color-light)' : 'var(--color-dark)';
+        el.style.backgroundColor = !!sections[currentSection].slide || currentSection === 7 ? 'var(--color-light)' : 'var(--color-dark)';
       });
     }
   }));
@@ -25,11 +25,11 @@ window.onload = () => {
     document.querySelector('.light-box').style.opacity = 0;
     document.querySelector('nav').style.width = 0;
     [...document.querySelector('.menu-button').children].forEach(el => {
-      el.style.backgroundColor = !!sections[currentSection].slide ? 'var(--color-light)' : 'var(--color-dark)';
+      el.style.backgroundColor = !!sections[currentSection].slide || currentSection === 7 ? 'var(--color-light)' : 'var(--color-dark)';
     });
   });
   document.querySelector('.to-top').addEventListener('click', () => {
-    sections[0].element.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+    sections[0].element.scrollIntoView({ behavior: "smooth", block: "end", inline: "start" });
     currentSection = 0;
     document.querySelector('.to-top').style.display = 'none';
     document.querySelector('nav').classList.remove('nav-mobile');
@@ -37,7 +37,7 @@ window.onload = () => {
     document.querySelector('nav').style.width = 'auto';
   });
   document.querySelector('#landing .scroll-down').addEventListener('click', () => {
-    sections[1].element.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+    sections[1].element.scrollIntoView({ behavior: "smooth", block: "end", inline: "start" });
     currentSection = 1;
     document.querySelector('.to-top').style.display = 'block';
     document.querySelector('.menu-button').style.display = 'block';
@@ -61,7 +61,7 @@ window.onload = () => {
       document.querySelector('.light-box').style.opacity = 0;
       document.querySelector('nav').style.width = 0;
       [...document.querySelector('.menu-button').children].forEach(el => {
-        el.style.backgroundColor = !!sections[currentSection].slide ? 'var(--color-light)' : 'var(--color-dark)';
+        el.style.backgroundColor = !!sections[currentSection].slide || currentSection === 7 ? 'var(--color-light)' : 'var(--color-dark)';
       });
     }
     else {
@@ -85,15 +85,15 @@ window.onload = () => {
     if (e.deltaY < 0 && currentSection - 1 >= 0 && !scrolled) {
       scrolled = true;
       setTimeout(() => { scrolled = false }, 800);
-      sections[--currentSection].element.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+      sections[--currentSection].element.scrollIntoView({ behavior: "smooth", block: "end", inline: "start" });
     } else if (e.deltaY > 0 && currentSection + 1 < sections.length && !scrolled) {
       scrolled = true;
       e.stopImmediatePropagation();
       setTimeout(() => { scrolled = false }, 800);
-      sections[++currentSection].element.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+      sections[++currentSection].element.scrollIntoView({ behavior: "smooth", block: "end", inline: "start" });
     }
     [...document.querySelector('.menu-button').children].forEach(el => {
-      el.style.backgroundColor = !!sections[currentSection].slide ? 'var(--color-light)' : 'var(--color-dark)';
+      el.style.backgroundColor = !!sections[currentSection].slide || currentSection === 7 ? 'var(--color-light)' : 'var(--color-dark)';
     });
     if(currentSection === 0) {
       document.querySelector('.to-top').style.display = 'none';
@@ -106,7 +106,7 @@ window.onload = () => {
       document.querySelector('.menu-button').style.display = 'block';
       document.querySelector('nav').style.width = 0;
     }
-    if(currentSection === 6)
+    if(currentSection === 6 || currentSection === 7)
       [...document.querySelector('.menu-button').children].forEach(el => {
         el.style.backgroundColor = 'var(--color-light)';
       });
@@ -118,7 +118,7 @@ window.onload = () => {
     startX = e.changedTouches[0].pageX;
   });
   window.addEventListener('resize', () => {
-    sections[currentSection].element.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+    sections[currentSection].element.scrollIntoView({ behavior: "smooth", block: "end", inline: "start" });
   });
   window.addEventListener('touchend', e => {
     var card = null;
@@ -128,41 +128,15 @@ window.onload = () => {
       card = e.target.parentNode;
     else if([...e.target.parentNode.parentNode.classList].includes('card-right'))
       card = e.target.parentNode.parentNode;
-    if(!!card && sc !== 0)
+    if(sc !== 0)
       sc = 0;
     else if(!!card)
       sc++;
-  });
-  window.addEventListener('touchmove', e => {
-    var card = null;
-    if([...e.target.classList].includes('card-right'))
-      card = e.target;
-    else if([...e.target.parentNode.classList].includes('card-right'))
-      card = e.target.parentNode;
-    else if([...e.target.parentNode.parentNode.classList].includes('card-right'))
-      card = e.target.parentNode.parentNode;
     document.querySelector('.menu-button').classList.remove('open');
     document.querySelector('.light-box').style.zIndex = 0;
     document.querySelector('.light-box').style.opacity = 0;
     document.querySelector('nav').style.width = 0;
     document.querySelectorAll('.gdg-logo img').forEach(el => el.style.transform = 'none');
-    var changeY = startY - e.changedTouches[0].pageY;
-    var changeX = startX - e.changedTouches[0].pageX;
-    var change = Math.abs(changeX) > Math.abs(changeY) ? changeX : changeY;
-    if(!!card && (change > 0 && card.scrollHeight - card.scrollTop - card.clientHeight > 0 || change < 0 && card.scrollTop > 0 || sc === 0))
-      return;
-    if (change < 0 && currentSection - 1 >= 0 && !scrolled) {
-      scrolled = true;
-      setTimeout(() => { scrolled = false }, 500);
-      sections[--currentSection].element.scrollIntoView({ behavior: "smooth", block: "end", inline: "start" });
-    } else if (change > 0 && currentSection + 1 < sections.length && !scrolled) {
-      scrolled = true;
-      setTimeout(() => { scrolled = false }, 500);
-      sections[++currentSection].element.scrollIntoView({ behavior: "smooth", block: "end", inline: "start" });
-    }
-    [...document.querySelector('.menu-button').children].forEach(el => {
-      el.style.backgroundColor = !!sections[currentSection].slide ? 'var(--color-light)' : 'var(--color-dark)';
-    });
     if(currentSection === 0) {
       document.querySelector('.to-top').style.display = 'none';
       document.querySelector('nav').classList.remove('nav-mobile');
@@ -174,9 +148,31 @@ window.onload = () => {
       document.querySelector('.menu-button').style.display = 'block';
       document.querySelector('nav').style.width = 0;
     }
-    if(currentSection === 6)
-      [...document.querySelector('.menu-button').children].forEach(el => {
-        el.style.backgroundColor = 'var(--color-light)';
-      });
+    [...document.querySelector('.menu-button').children].forEach(el => {
+      el.style.backgroundColor = !!sections[currentSection].slide || currentSection === 7 ? 'var(--color-light)' : 'var(--color-dark)';
+    });
+  });
+  window.addEventListener('touchmove', e => {
+    var card = null;
+    if([...e.target.classList].includes('card-right'))
+      card = e.target;
+    else if([...e.target.parentNode.classList].includes('card-right'))
+      card = e.target.parentNode;
+    else if([...e.target.parentNode.parentNode.classList].includes('card-right'))
+      card = e.target.parentNode.parentNode;
+    var changeY = startY - e.changedTouches[0].pageY;
+    var changeX = startX - e.changedTouches[0].pageX;
+    var change = Math.abs(changeX) > Math.abs(changeY) ? changeX : changeY;
+    if(!!card && (change > 0 && card.scrollHeight - card.scrollTop - card.clientHeight > 0 || change < 0 && card.scrollTop > 0 || sc === 0))
+      return;
+    if (change < 0 && currentSection - 1 >= 0 && !scrolled) {
+      scrolled = true;
+      setTimeout(() => { scrolled = false }, 200);
+      sections[--currentSection].element.scrollIntoView({ behavior: "smooth", block: "end", inline: "start" });
+    } else if (change > 0 && currentSection + 1 < sections.length && !scrolled) {
+      scrolled = true;
+      setTimeout(() => { scrolled = false }, 200);
+      sections[++currentSection].element.scrollIntoView({ behavior: "smooth", block: "end", inline: "start" });
+    }
   });
 }
